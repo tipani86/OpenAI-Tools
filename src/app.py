a42710aa23ef2891452ed20498be4c92490fc369
Tianyi Pan <tianyi.pan@clobotics.com>
2023-09-21 16:11:20 +0800

    Quick fix to filter 'instruct' GPT models as they don't adhere to chat format

--- src/app.py
+++ src/app.py
@@ -497,7 +497,7 @@ async def main():
 
             with st.form("prompt_form", clear_on_submit=False):
                 st.caption("**Test Model Performance (Limited to Chat Models)**")
-                usable_models = [value for value in models_df["id"].values if "gpt" in value]
+                usable_models = [value for value in models_df["id"].values if "gpt" in value and "instruct" not in value]
                 model_id = st.selectbox("Model ID", usable_models, index=len(usable_models) - 1)
                 system_prompt = st.text_input("System Prompt (Optional)", help="You can use the System Prompt to guide model behavior by giving it some instructions.")
                 prompt = st.text_area("Your Message")
